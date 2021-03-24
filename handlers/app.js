aa4b7682abb042f1607f6aa470557e1c53bafe63
SurajGupta07 <48823022+SurajGupta07@users.noreply.github.com>
2021-03-24 10:19:22 +0530

    Component Library

---
+++ handlers/app.js
@@ -0,0 +1,10 @@
+$(function () {
+    $(".load-avatar").load("/views/avatar.html");
+    $(".load-alert").load("/views/alert.html");
+    $(".load-badge").load("/views/badge.html");
+    $(".load-button").load("/views/buttons.html");
+    $(".load-card").load("/views/card.html");
+    $(".load-image").load("/views/images.html");
+    $(".load-input").load("/views/input.html");
+    $(".load-list").load("/views/list.html");
+});
